4ae93f391c97d8495d866481a47cdee46d75a26e
tompassarelli <tom.passarelli@protonmail.com>
2025-10-30 00:58:12 -0400

    show waybar in overview, rename workspace

--- dotfiles/waybar/overview-waybar.py
+++ dotfiles/waybar/overview-waybar.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 from json import loads
 from os import environ
-from signal import SIGUSR1
-from subprocess import Popen, DEVNULL, run
+from subprocess import run
 from socket import AF_UNIX, socket, SHUT_WR
 import sys
 
 def main():
-    waybar_proc = None
     try:
         niri_socket = socket(AF_UNIX)
         niri_socket.connect(environ["NIRI_SOCKET"])
@@ -15,13 +13,6 @@ def main():
         file.write('"EventStream"')
         file.flush()
         niri_socket.shutdown(SHUT_WR)
-
-        # Start waybar (it will start hidden due to config)
-        waybar_proc = Popen(
-            ["waybar", "--config", f"{environ['HOME']}/.config/waybar/config", "--style", f"{environ['HOME']}/.config/waybar/style.css"],
-            stdout=DEVNULL,
-            stderr=DEVNULL
-        )
 
         for line in file:
             event = loads(line)
@@ -31,8 +22,6 @@ def main():
                 
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
-        if waybar_proc:
-            waybar_proc.terminate()
         sys.exit(1)
 
 if __name__ == "__main__":
