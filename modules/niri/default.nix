aa2087fe4b6bc0d181192ff5585ee25adda93b40
tompassarelli <tom.passarelli@protonmail.com>
2025-11-27 20:01:04 -0500

    updoot

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -6,7 +6,7 @@
 
   imports = [
     ./niri.nix
-    ./xwayland-satellite.nix
+    ./xwayland-satellite.nix  # Niri 25.08 needs this in PATH, manages it automatically
     ./swaybg.nix
     ./swayidle.nix
   ];
--- modules/niri/xwayland-satellite.nix
+++ modules/niri/xwayland-satellite.nix
@@ -1,26 +1,10 @@
 { config, lib, pkgs, username, ... }:
 {
   config = lib.mkIf config.myConfig.niri.enable {
+    # Install xwayland-satellite for niri to use
+    # Niri 25.08+ manages xwayland-satellite automatically - no systemd service needed
     environment.systemPackages = with pkgs; [
-      xwayland-satellite  # required for some apps like 2025 bitwarden
+      unstable.xwayland-satellite
     ];
-
-    # Run xwayland-satellite as a systemd user service
-    home-manager.users.${username} = {
-      systemd.user.services.xwayland-satellite = {
-        Unit = {
-          Description = "XWayland Satellite for Niri";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-        };
-        Service = {
-          ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite";
-          Restart = "on-failure";
-        };
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
-      };
-    };
   };
 }
