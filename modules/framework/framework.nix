75a9b3f0dbd2637296198a37bf68fd926910608f
tompassarelli <tom.passarelli@protonmail.com>
2025-12-03 02:51:31 -0500

    more agressive wifi fix

--- modules/framework/framework.nix
+++ modules/framework/framework.nix
@@ -10,10 +10,12 @@ in
       fwupd                # update drivers/bios
     ];
 
-    # Disable CLC (Country Location Code) auto-detection
-    # Helps with mt7925e stability issues on 6GHz band
+    # MT7925e WiFi stability fixes
+    # - disable_clc: Disables Country Location Code auto-detection (6GHz issues)
+    # - disable_aspm: Disables PCI power management (prevents race conditions during roaming)
     boot.extraModprobeConfig = ''
       options mt7925_common disable_clc=1
+      options mt7925e disable_aspm=1
     '';
   };
 }
