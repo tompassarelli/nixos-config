8ddfe3ff6b3cd0542d530cb2ff95abb218379c37
tompassarelli <tom.passarelli@protonmail.com>
2025-12-03 02:32:42 -0500

    stop wifi from deadlocking the kernel

--- modules/framework/framework.nix
+++ modules/framework/framework.nix
@@ -9,5 +9,11 @@ in
       framework-tool       # swiss army knife
       fwupd                # update drivers/bios
     ];
+
+    # Disable CLC (Country Location Code) auto-detection
+    # Helps with mt7925e stability issues on 6GHz band
+    boot.extraModprobeConfig = ''
+      options mt7925_common disable_clc=1
+    '';
   };
 }
